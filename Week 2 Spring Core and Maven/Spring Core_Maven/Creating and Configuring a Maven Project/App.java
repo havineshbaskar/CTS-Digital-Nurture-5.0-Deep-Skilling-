fd5b0d257b60125b11e5f9406dd829588e27bd20
HAVINESH D <havineshbaskar@gmail.com>
2026-06-29 16:12:34 +0530

    Add files via upload

---
+++ Week 2 Spring Core and Maven/Spring Core_Maven/Creating and Configuring a Maven Project/App.java
@@ -0,0 +1,19 @@
+package com.example;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class App {
+
+    public static void main(String[] args) {
+
+        ApplicationContext context =
+                new ClassPathXmlApplicationContext("applicationContext.xml");
+
+        Library library = context.getBean("library", Library.class);
+
+        library.showBooks();
+
+        ((ClassPathXmlApplicationContext) context).close();
+    }
+}
---
+++ Week 2 Spring Core and Maven/Spring Core_Maven/Creating and Configuring a Maven Project/Library.java
@@ -0,0 +1,11 @@
+package com.example;
+
+public class Library {
+
+    public void showBooks() {
+        System.out.println("Available Books:");
+        System.out.println("Book-1. Java Programming");
+        System.out.println("Book-2. Spring Framework");
+        System.out.println("Book-3. Database Systems");
+    }
+}
