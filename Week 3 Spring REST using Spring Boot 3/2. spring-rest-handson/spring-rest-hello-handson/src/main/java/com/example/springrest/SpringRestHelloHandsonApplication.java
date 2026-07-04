52a496b9ca9a35dd580e6b591e106923842f3456
HAVINESH D <havineshbaskar@gmail.com>
2026-07-04 11:44:14 +0530

    Add files via upload

---
+++ Week 3 Spring REST using Spring Boot 3/2. spring-rest-handson/spring-rest-hello-handson/src/main/java/com/example/springrest/SpringRestHelloHandsonApplication.java
@@ -0,0 +1,13 @@
+package com.example.springrest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringRestHelloHandsonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringRestHelloHandsonApplication.class, args);
+	}
+
+}
