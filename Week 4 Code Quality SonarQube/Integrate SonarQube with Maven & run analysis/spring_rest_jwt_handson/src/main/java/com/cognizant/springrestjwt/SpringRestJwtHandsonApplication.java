07f0eb36805472051fa79eca01884640fc93cd4b
HAVINESH D <havineshbaskar@gmail.com>
2026-07-04 15:59:32 +0530

    Add files via upload

---
+++ Week 4 Code Quality SonarQube/Integrate SonarQube with Maven & run analysis/spring_rest_jwt_handson/src/main/java/com/cognizant/springrestjwt/SpringRestJwtHandsonApplication.java
@@ -0,0 +1,12 @@
+package com.cognizant.springrestjwt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringRestJwtHandsonApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringRestJwtHandsonApplication.class, args);
+    }
+}
